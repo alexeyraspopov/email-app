89c3df70d030476183391fb01a508fe52f668c9e
Avenger7x <avenger7x13@gmail.com>
2014-09-04 16:35:49 +0300

    dev environment

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+	browserify = require('gulp-browserify'),
+	rename = require('gulp-rename'),
+	clean = require('gulp-clean');
+
+gulp.task('clean', function(){
+	return gulp.src('dist', { read: false })
+		.pipe(clean());
+});
+
+gulp.task('default', function(){
+	return gulp.src('src/index.jsx')
+		.pipe(browserify({
+			transform: ['reactify']
+		}))
+		.pipe(rename('app.js'))
+		.pipe(gulp.dest('dist'));
+});
